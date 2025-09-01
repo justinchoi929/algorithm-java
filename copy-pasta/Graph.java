c53dd3701d66e6f447788d07b8a0609b6c9a192a
JustinChoi <2813970520@qq.com>
2025-09-01 21:01:53 +0800

    网格图 BFS

--- copy-pasta/Graph.java
+++ copy-pasta/Graph.java
@@ -44,4 +44,40 @@ class Graph {
         }
         return size;
     }
+
+    /**
+     * 网格图 BFS
+     * 适用于需要计算最短距离（最短路）的题目
+     */
+    public int bfs(char[][] maze, int[] entrance) {
+        int m = maze.length;
+        int n = maze[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] { entrance[0], entrance[1] });
+        visited[entrance[0]][entrance[1]] = true;
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            ans++;
+            Deque<int[]> curs = queue;
+            queue = new ArrayDeque<>();
+            for (int[] cur : curs) {
+                int x = cur[0];
+                int y = cur[1];
+                for (int[] dir : DIRS) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && maze[nx][ny] == '.' && !visited[nx][ny]) {
+                        if (nx == 0 || ny == 0 || nx == m - 1 || ny == n - 1) {
+                            return ans;
+                        }
+                        visited[nx][ny] = true;
+                        queue.offer(new int[] { nx, ny });
+                    }
+                }
+            }
+
+        }
+        return -1;
+    }
 }
