59681b2e9292a9e1a76d14206dbd0ee3bb60869e
JustinChoi <2813970520@qq.com>
2025-09-02 23:25:03 +0800

    网格图 Dijkstra

--- copy-pasta/Graph.java
+++ copy-pasta/Graph.java
@@ -29,7 +29,7 @@ class Graph {
         return size;
     }
     // 也可写成如下形式
-    private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}; // 左右上下
+    private static final int[][] dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}; // 左右上下
     private int dfs(int[][] grid, int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
             return 0; // 越界返回 0 而不是 size
@@ -37,7 +37,7 @@ class Graph {
         grid[i][j] = 0;
         int size = 1;
 
-        for (int[] d : DIRS) {
+        for (int[] d : dirs) {
             int x = i + d[0];
             int y = j + d[1];
             size += dfs(grid, x, y);
@@ -49,35 +49,123 @@ class Graph {
      * 网格图 BFS
      * 适用于需要计算最短距离（最短路）的题目
      */
+    private static final int[][] dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}; // 左右上下
     public int bfs(char[][] maze, int[] entrance) {
         int m = maze.length;
         int n = maze[0].length;
         boolean[][] visited = new boolean[m][n];
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[] { entrance[0], entrance[1] });
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] { entrance[0], entrance[1] });
         visited[entrance[0]][entrance[1]] = true;
         int ans = 0;
-        while (!queue.isEmpty()) {
+        while (!q.isEmpty()) {
             ans++;
-            Deque<int[]> curs = queue;
-            queue = new ArrayDeque<>();
+            Deque<int[]> curs = q;
+            q = new ArrayDeque<>();
             for (int[] cur : curs) {
-                int x = cur[0];
-                int y = cur[1];
-                for (int[] dir : DIRS) {
-                    int nx = x + dir[0];
-                    int ny = y + dir[1];
-                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && maze[nx][ny] == '.' && !visited[nx][ny]) {
-                        if (nx == 0 || ny == 0 || nx == m - 1 || ny == n - 1) {
+                for (int[] dir : dirs) {
+                    int x = cur[0] + dir[0];
+                    int y = cur[1] + dir[1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == '.' && !visited[x][y]) {
+                        if (x == 0 || y == 0 || x == m - 1 || y == n - 1) {
                             return ans;
                         }
-                        visited[nx][ny] = true;
-                        queue.offer(new int[] { nx, ny });
+                        visited[x][y] = true;
+                        q.offer(new int[] { x, y });
                     }
                 }
             }
-
         }
         return -1;
     }
+
+    /**
+     * 网格图 Dijkstra 算法（朴素版）
+     * 1. 选距离源点最近且未访问的点
+     * 2. 标记该点访问过
+     * 3. 更新所有未访问节点到源点的距离
+     */
+    public int networkDelayTime(int[][] times, int n, int k) {
+        final int inf = Integer.MAX_VALUE / 2;
+        int[][] g = new int[n][n]; // 存储从 i 到 j 的边权
+        boolean[] visited = new boolean[n];
+        for (int[] row : g) {
+            Arrays.fill(row, inf);
+        }
+        for (int[] t : times) {
+            g[t[0] - 1][t[1] - 1] = t[2];
+        }
+
+        int[] dis = new int[n]; // dis[i] 表示 i 到源点的最短路径
+        Arrays.fill(dis, inf);
+        dis[k - 1] = 0;
+        int minDis = inf;
+        for (int i = 0; i < n; i++) {
+            minDis = inf;
+            int cur = 0;
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && dis[j] < minDis) {
+                    minDis = dis[j];
+                    cur = j;
+                }
+            }
+            if (minDis == inf) {
+                return -1;
+            }
+
+            visited[cur] = true;
+            for (int j = 0; j < n; j++) {
+                if (!visited[j]) {
+                    dis[j] = Math.min(minDis + g[cur][j], dis[j]);
+                }
+            }
+        }
+
+        return minDis;
+    }
+
+    /**
+     * 网格图 Dijkstra 算法（堆优化版）
+     * 1. 选距离源点最近且未访问的点
+     * 2. 标记该点访问过
+     * 3. 更新所有未访问节点到源点的距离
+     */
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<int[]>[] g = new ArrayList[n]; // 邻接表存储图
+        Arrays.setAll(g, i -> new ArrayList<>());
+        int[] dis = new int[n];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for (int[] t : times) {
+            g[t[0] - 1].add(new int[] { t[1] - 1, t[2] });
+        }
+
+        dis[k - 1] = 0;
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+        boolean[] visited = new boolean[n];
+        q.offer(new int[] { k - 1, 0 });
+        int minDis = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            minDis = cur[1];
+            if (visited[x] == true) {
+                continue;
+            }
+            visited[x] = true;
+            for (int[] e : g[x]) {
+                int y = e[0];
+                if (minDis + e[1] < dis[y]) {
+                    dis[y] = minDis + e[1];
+                    q.offer(new int[] { y, dis[y] });
+                }
+            }
+        }
+        int ans = Arrays.stream(dis).max().getAsInt();
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    /**
+     * 网格图 0-1 BFS
+     */
 }
